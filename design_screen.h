6d1b9bc27b5249e2f2abad4817c5835053e18b1b
fatemehtashtaryan <ffaatteemmeehhtashtaryan@gmail.com>
2024-01-06 09:50:23 +0330

    arranging several functions

--- design_screen.h
+++ design_screen.h
@@ -1,8 +1,10 @@
 #include "subcodes.h"
-void Design_color_screen()
+
+//This file consists of a number of functions that can be seen at the beginning of the game and the last function is for printing the winner
+
+void Design_color_screen()//Choice of screen color by players
 {
 printf ("'SELECT YOUR SCREEN COLOR BY PRESS ENTER THEN PRESS 0'\n") ;
-    char charnumber=(char)(0+'0');
     while ( getch() == 13 )
         {
          beep (500,100) ;
@@ -42,7 +44,7 @@ void loading(){
      		y+=1;
      	    gotoxy(x, y);
      	    setTextColor(2,0);
-     	    printf("%c",223);
+     	    printf("%c",223);//rectangle ascii
 		 }
 		 gotoxy(x, y+1);
          printf(" %d %c",t,37);
@@ -57,6 +59,100 @@ void loading(){
     	sleep(50);
 }
 
+int logo(int TotalRows,int TotalColumn){
+     int  Row, Column ,i;
+     for(i=1;i<=5;i++){
+				board[4][i]=100;
+		}
+     for(i=9;i<=13;i++){
+				board[4][i]=200;
+		}
+     for(i=1;i<=3;i++){
+				board[i][7]=300;
+		}
+     for(i=5;i<=7;i++){
+				board[i][7]=400;
+		}
+	 for(Row = 0; Row <= TotalRows-1 ; Row++){
+	    for(Column = 0; Column <= TotalColumn-1 ; Column++){
+	    	//////////print 4 corners of the frame
+			if(Row==0 && Column==0){
+					for(i=1 ;i <=55 ;i++){
+								printf(" ");
+							}
+					printf("%c",218);
+			}
+			if(Row==0 && Column==TotalColumn-2){
+					printf("%c",191);
+			}
+			if(Row==TotalRows-1 && Column==TotalColumn-2){
+					printf("%c",217);
+			}
+			if(Row==TotalRows-1 && Column==0){
+					for( i=1 ;i<=55;i++){
+								printf(" ");
+							}
+					printf("%c",192);
+			}
+			////////////// print houses
+	        if(board[Row][Column] == 0 ){
+	        	printf(" ");
+			}
+	        else if(board[Row][Column] == 1   ) {
+                printf("%c",205);
+			}
+			else if(board[Row][Column] ==2  ) {
+	        	printf("%c",215);
+			}
+			else if(board[Row][Column] ==3  ) {
+	        	printf("%c",201);
+			}
+			else if(board[Row][Column] ==4) {
+	        	printf("%c",187);
+			}
+			else if(board[Row][Column] ==5 ) {
+	        	printf("%c",200);
+			}
+			else if(board[Row][Column] ==6  ) {
+	        	printf("%c",188);
+			}
+			////////// print main frame
+			else if(board[Row][Column]==7  )	{
+						if(Column==0){
+							for( i=1 ;i<=55;i++){
+								printf(" ");
+							}
+						}
+						printf("%c",179);
+			}
+		    else if(board[Row][Column]==8 )	{
+                            printf("%c",196);
+		    }
+		    if(board[Row][Column]==100){
+                 setTextColor(2,0);
+                printf("%c",178);
+                  setTextColor(3,0);
+                }
+            if(board[Row][Column]==200){
+                 setTextColor(6,0);
+                printf("%c",178);
+                  setTextColor(3,0);
+                }
+            if(board[Row][Column]==300){
+                 setTextColor(4,0);
+                printf("%c",178);
+                  setTextColor(3,0);
+                }
+            if(board[Row][Column]==400){
+                 setTextColor(5,0);
+                printf("%c",178);
+                  setTextColor(3,0);
+                }
+	    }
+	     printf("\n");
+	 }
+}
+
 void printwon(char name[])
 {
     int n ;
@@ -68,6 +164,3 @@ void printwon(char name[])
         sleep (30) ;
     }
 }
-
-
-
