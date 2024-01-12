0e4b7dd5f387c0362114bd28dec45184a950800f
fatemehtashtaryan <ffaatteemmeehhtashtaryan@gmail.com>
2024-01-12 11:58:56 +0330

    changing bets due to the addition of computer moves

--- checking_move.h
+++ checking_move.h
@@ -2,7 +2,7 @@
 #define checking_move
 
 #include "subcodes.h"
-#include "conio.h"
+
 //up_move=72;
 // down_move=80
 // left_move=75;
@@ -19,7 +19,7 @@ int check_move (int type_of_movement)
       row = row1;
       full_house = 300;
    }
-   else {//turn == 2;
+   else {//turn == 2 OR turn == 3;
       beat = 300;
       column = column2;
       row = row2;
@@ -29,26 +29,30 @@ int check_move (int type_of_movement)
      case 72 :
         if (board[row-2][column] == 1000 || board[row-2][column] == 8 || board[row-2][column] == 2000)//check sticks & walls
         {
-            beep(500, 100);
-            gotoxy(x, 44);
-            setTextColor(12, colorscreen);
-            printf("your way is C L O S E D!");
-            setTextColor(0, colorscreen);
+            if(turn != 3){
+              beep(500, 100);
+              gotoxy(x, 44);
+              setTextColor(12, colorscreen);
+              printf("your way is C L O S E D!");
+              setTextColor(0, colorscreen);
+            }
             return 0;
         }
         else if (board[row-4][column] == full_house )//check empty house
         {
-            beep(500, 100);
-            gotoxy(x, 44);
-            setTextColor(12, colorscreen);
-            printf("house is F U L L!");
-            setTextColor(0, colorscreen);
+            if(turn != 3){
+              beep(500, 100);
+              gotoxy(x, 44);
+              setTextColor(12, colorscreen);
+              printf("house is F U L L!");
+              setTextColor(0, colorscreen);
+            }
             return 0;
         }
         else
         {
             board[row][column] = 0 ;
-            board[row-4][column] = 200 ;//placing beat
+            board[row-4][column] = beat ;//placing beat
             row -= 4 ;
              if (turn == 1){
                column1 = column;
@@ -65,20 +69,24 @@ int check_move (int type_of_movement)
     case 80 :
         if ( board[row+2][column] == 1000 || board[row+2][column] == 8 || board[row+2][column] == 2000)//check sticks & walls
         {
-            beep (500, 100);
-            gotoxy (x, 44);
-            setTextColor (12, colorscreen);
-            printf ("your way is C L O S E D!");
-            setTextColor(0, colorscreen);
+            if(turn != 3){
+              beep (500, 100);
+              gotoxy (x, 44);
+              setTextColor (12, colorscreen);
+              printf ("your way is C L O S E D!");
+              setTextColor(0, colorscreen);
+            }
             return 0;
         }
         else if (board[row+4][column] == full_house )//check empty house
         {
-            beep (500, 100);
-            gotoxy (x, 44);
-            setTextColor (12, colorscreen);
-            printf ("house is F U L L!");
-            setTextColor(0, colorscreen);
+            if(turn != 3){
+              beep (500, 100);
+              gotoxy (x, 44);
+              setTextColor (12, colorscreen);
+              printf ("house is F U L L!");
+              setTextColor(0, colorscreen);
+            }
             return 0;
         }
         else
@@ -90,7 +98,7 @@ int check_move (int type_of_movement)
                column1 = column;
                row1 = row;
             }
-           else {//turn == 2;
+           else {//turn == 2; OR turn ==3
              column2 = column;
              row2 = row;
            }
@@ -101,20 +109,24 @@ int check_move (int type_of_movement)
     case 77:
         if (board[row][column+4] ==1000 || board[row][column+3]==7 || board[row][column+4] ==2000)//check sticks & walls
             {
-            beep(500, 100);
-            gotoxy(x, 44);
-            setTextColor(42, colorscreen);
-            printf("your way is C L O S E D!");
-            setTextColor(0, colorscreen);
+            if(turn != 3){
+              beep(500, 100);
+              gotoxy(x, 44);
+              setTextColor(12, colorscreen);
+              printf("your way is C L O S E D!");
+              setTextColor(0, colorscreen);
+           }
             return 0;
         }
         else if (board[row][column+8] == full_house)//check empty house
-            {
-            beep(500, 100);
-            gotoxy(x, 44);
-            setTextColor(42, colorscreen);
-            printf("house is F U L L!");
-            setTextColor(0, colorscreen);
+           {
+            if(turn != 3){
+                beep(500, 100);
+                gotoxy(x, 44);
+                setTextColor(12, colorscreen);
+                printf("house is F U L L!");
+                setTextColor(0, colorscreen);
+            }
             return 0;
         }
         else{
@@ -125,7 +137,7 @@ int check_move (int type_of_movement)
                column1 = column;
                row1 = row;
             }
-           else {//turn == 2;
+           else {//turn == 2; OR turn ==3
              column2 = column;
              row2 = row;
            }
@@ -136,20 +148,24 @@ int check_move (int type_of_movement)
     case 75:
         if (board[row][column-4] == 1000 || board[row][column-3] == 7 || board[row][column-4] == 2000)//check sticks & walls
             {
-            beep(500, 100);
-            gotoxy(x, 44);
-            setTextColor(12, colorscreen);
-            printf("your way is C L O S E D!");
-            setTextColor(0, colorscreen);
+            if(turn != 3){
+              beep(500, 100);
+              gotoxy(x, 44);
+              setTextColor(12, colorscreen);
+              printf("your way is C L O S E D!");
+              setTextColor(0, colorscreen);
+            }
             return 0;
         }
         else if(board[row][column-8] == full_house)//check empty house
             {
-            beep(500, 100);
-            gotoxy(x, 44);
-            setTextColor(12, colorscreen);
-            printf("house is F U L L!");
-            setTextColor(0, colorscreen);
+            if(turn != 3){
+              beep(500, 100);
+              gotoxy(x, 44);
+              setTextColor(12, colorscreen);
+              printf("house is F U L L!");
+              setTextColor(0, colorscreen);
+            }
             return 0;
         }
         else{
@@ -160,7 +176,7 @@ int check_move (int type_of_movement)
                column1 = column;
                row1 = row;
             }
-         else {//turn == 2;
+         else {//turn == 2 OR turn ==3
              column2 = column;
              row2 = row;
           }
