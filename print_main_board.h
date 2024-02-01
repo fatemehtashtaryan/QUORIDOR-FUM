6bebb2333b35e7b7b8a89492af2a7bc622d1faec
fatemehtashtaryan <ffaatteemmeehhtashtaryan@gmail.com>
2024-02-02 01:19:31 +0330

    new changes for setting

--- print_main_board.h
+++ print_main_board.h
@@ -3,7 +3,19 @@
 //this file is about print main board and colores around houses
 
 void printboardgame(TotalRows, TotalColumn){
+    int  Rowcopy, Columncopy;
+    if(turn==3){
+    for( Rowcopy = 0;  Rowcopy < TotalRows ;  Rowcopy++){
+        for(Columncopy = 0; Columncopy < TotalColumn ; Columncopy++){
 
+                copy_board.copyboard[ Rowcopy][Columncopy]=board[ Rowcopy][Columncopy];
+        }
+    }
+     copy_board.copyverticalsticks1=verticalsticks1;
+     copy_board.copyverticalsticks2=verticalsticks2;
+     copy_board.copyhorizontalsticks1=horizontalsticks1;
+     copy_board.copyhorizontalsticks2=horizontalsticks2;
+    }
      board[row1-1][column1-2]=30;
      board[row1-1][column1+2]=40;
      board[row1+1][column1-2]=50;
@@ -38,106 +50,106 @@ void printboardgame(TotalRows, TotalColumn){
      if(board[row2+2][column2] != 1000 && board[row2+2][column2] != 2000 && board[row2+2][column2] != 8 && board[row2+4][column2] != 200 && turn == 2) board[row2+4][column2] =400;
      if(board[row2-2][column2] != 1000 && board[row2-2][column2] != 2000 && board[row2-2][column2] != 8 && board[row2-4][column2] != 200 && turn == 2) board[row2-4][column2] =400;
 
-	 int  Row, Column, sw=1 ,i;
-	 for(Row = 0; Row <= TotalRows ; Row++){
-	    for(Column = 0; Column <= TotalColumn; Column++){
-	    	//////////print 4 corners of the frame
-			if(Row==0 && Column==0){
-					for(i=1 ;i <=4*(14-size_board) ;i++){
-								printf(" ");
-							}
-					printf("%c",218);
-			}
-			if(Row==0 && Column==TotalColumn-2){
-					printf("%c",191);
-			}
-			if(Row==TotalRows-1 && Column==TotalColumn-2){
-					printf("%c",217);
-			}
-			if(Row==TotalRows-1 && Column==0){
-					for( i=1 ;i<=4*(14-size_board);i++){
-								printf(" ");
-							}
-					printf("%c",192);
-			}
+int  Row, Column, sw=1 ,i;
+for(Row = 0; Row <= TotalRows ; Row++){
+   for(Column = 0; Column <= TotalColumn; Column++){
+    //////////print 4 corners of the frame
+if(Row==0 && Column==0){
+for(i=1 ;i <=4*(14-size_board) ;i++){
+printf(" ");
+}
+printf("%c",218);
+}
+if(Row==0 && Column==TotalColumn-2){
+printf("%c",191);
+}
+if(Row==TotalRows-1 && Column==TotalColumn-2){
+printf("%c",217);
+}
+if(Row==TotalRows-1 && Column==0){
+for( i=1 ;i<=4*(14-size_board);i++){
+printf(" ");
+}
+printf("%c",192);
+}
 
-			////////////// print houses
-	        if(board[Row][Column] == 0 && Row!=TotalRows){
-	        	printf(" ");
-			}
-	        else if(board[Row][Column] == 1 && Row!=TotalRows && Column!=TotalColumn) {
+////////////// print houses
+       if(board[Row][Column] == 0 && Row!=TotalRows){
+        printf(" ");
+}
+       else if(board[Row][Column] == 1 && Row!=TotalRows && Column!=TotalColumn) {
                 printf("%c",205);
-			}
-			else if(board[Row][Column] ==2 && Row!=TotalRows && Column!=TotalColumn) {
-	        	printf("%c",215);
-			}
-			else if(board[Row][Column] ==3 && Row!=TotalRows && Column!=TotalColumn) {
-	        	printf("%c",201);
-			}
-			else if(board[Row][Column] ==4 && Row!=TotalRows && Column!=TotalColumn) {
-	        	printf("%c",187);
-			}
-			else if(board[Row][Column] ==5 && Row!=TotalRows && Column!=TotalColumn) {
-	        	printf("%c",200);
-			}
-			else if(board[Row][Column] ==6 && Row!=TotalRows && Column!=TotalColumn) {
-	        	printf("%c",188);
-			}
-			////////// print main frame
-			else if(board[Row][Column]==7 && Column!=TotalColumn && Row!=TotalRows )	{
-						if(Column==0){
-							for( i=1 ;i<=4*(14-size_board);i++){
-								printf(" ");
-							}
+}
+else if(board[Row][Column] ==2 && Row!=TotalRows && Column!=TotalColumn) {
+        printf("%c",215);
+}
+else if(board[Row][Column] ==3 && Row!=TotalRows && Column!=TotalColumn) {
+        printf("%c",201);
+}
+else if(board[Row][Column] ==4 && Row!=TotalRows && Column!=TotalColumn) {
+        printf("%c",187);
+}
+else if(board[Row][Column] ==5 && Row!=TotalRows && Column!=TotalColumn) {
+        printf("%c",200);
+}
+else if(board[Row][Column] ==6 && Row!=TotalRows && Column!=TotalColumn) {
+        printf("%c",188);
+}
+////////// print main frame
+else if(board[Row][Column]==7 && Column!=TotalColumn && Row!=TotalRows ) {
+if(Column==0){
+for( i=1 ;i<=4*(14-size_board);i++){
+printf(" ");
+}
 
-						}
-						printf("%c",179);
-			}
-		    else if(board[Row][Column]==8 && Column!=TotalColumn && Row!=TotalRows )	{
-						if(Row == 0){
+}
+printf("%c",179);
+}
+   else if(board[Row][Column]==8 && Column!=TotalColumn && Row!=TotalRows ) {
+if(Row == 0){
                             setTextColor(9, colorscreen);
                             printf("%c",196);
                             setTextColor(0, colorscreen);
-						}
-						else if(Row == TotalRows -1){
+}
+else if(Row == TotalRows -1){
                             setTextColor(13, colorscreen);
                             printf("%c",196);
                             setTextColor(0, colorscreen);
-						}
-						else printf("%c",196);
-			}
-			///////////print line nums.
-	        if(Column==TotalColumn && board[Row][Column]!=0 ){
-						if(board[Row][Column]>64){
-								printf("%c",board[Row][Column]);
-						}else{
-							printf("%d",board[Row][Column]);
-						}
-			}
-			/////////print Column nums.
-			if(Row==TotalRows){
-				if(sw==1){
-					for( i=1 ;i<=4*(14-size_board);i++){
-						printf(" ");
-					}
-					sw=0;
-				}
-					if(board[Row][Column]!=0 && Column<TotalColumn){
-						if(board[Row][Column]>64){
+}
+else printf("%c",196);
+}
+///////////print line nums.
+       if(Column==TotalColumn && board[Row][Column]!=0 ){
+if(board[Row][Column]>64){
+printf("%c",board[Row][Column]);
+}else{
+printf("%d",board[Row][Column]);
+}
+}
+/////////print Column nums.
+if(Row==TotalRows){
+if(sw==1){
+for( i=1 ;i<=4*(14-size_board);i++){
+printf(" ");
+}
+sw=0;
+}
+if(board[Row][Column]!=0 && Column<TotalColumn){
+if(board[Row][Column]>64){
 
-								printf("%c",board[Row][Column]);
-						}
-						else
-						{
-								printf("%d",board[Row][Column]);
-								}
-					}
-					else{
-						printf(" ");
-					}
-				}
-	      /////////////print sticks
-	        if(board[Row][Column] == 1000)
+printf("%c",board[Row][Column]);
+}else{
+
+printf("%d",board[Row][Column]);
+}
+
+}
+else{
+printf(" ");
+}
+}
+     /////////////print sticks
+       if(board[Row][Column] == 1000)
             {
                 setTextColor(13, colorscreen);
                 printf("%c",178);}
@@ -146,9 +158,9 @@ void printboardgame(TotalRows, TotalColumn){
                 setTextColor(9, colorscreen);
                 printf("%c",178);
             }
-            ////////////
-	        setTextColor(0, colorscreen);
-		 	if(board[Row][Column] == 200) printf("%c", beat1);
+            ////////////print  beats
+       setTextColor(0, colorscreen);
+if(board[Row][Column] == 200) printf("%c", beat1);
             if(board[Row][Column] == 300) printf("%c", beat2);
             //player 1 color around house
             setTextColor(13,colorscreen);
@@ -177,8 +189,7 @@ void printboardgame(TotalRows, TotalColumn){
             if(board[Row][Column] == 63 || board[Row][Column] == 64)  board[Row][Column] = 1;
             if(board[Row][Column] == 62 || board[Row][Column] == 61)  board[Row][Column] = 2;
             if(board[Row][Column] == 400)  board[Row][Column] = 0;
-	    }
-	    printf("\n");
-	}
+   }
+   printf("\n");
 }
-
+}
