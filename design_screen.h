7e0cb2ee0aed67c9df716f48cf6de8ccdea226a6
fatemehtashtaryan <ffaatteemmeehhtashtaryan@gmail.com>
2024-02-02 01:45:43 +0330

    new changes in screen color function & add size board function for setting

--- design_screen.h
+++ design_screen.h
@@ -1,19 +1,186 @@
-#include "subcodes.h"
+#ifndef design_screen
+#define design_screen
 
+#include "subcodes.h"
+#include "setting.h"
 //This file consists of a number of functions that can be seen at the beginning of the game and the last function is for printing the winner
+
+void sizeboard(){
+      char setting_direction;
+      int number=0, type=1, sw_setting=0;
+      size_board=5;
+      setTextColor (7,0);
+      frame(8,39 ,35,10);
+      gotoxy(4,45); printf("Q U O R I D O R   SETTING!");
+      gotoxy(10,41); printf ("'CHOOSE THE BOARD SIZE YOU WANT'") ;
+      setTextColor(7,0); setting_shape(13, 41, 9); gotoxy(15,45); printf("<<<");
+      setTextColor(7,0); setting_shape(13, 52, 9); gotoxy(15,57); printf("%d", size_board);
+      setTextColor (7,0); setting_shape(13, 63, 9); gotoxy(15,67); printf(">>>");
+      setTextColor (7,0); setting_shape(20, 51, 11); gotoxy(22,56); printf("EXIT");
+      do{
+        while(1){
+            setting_direction=getch();
+            if(setting_direction==13) break;
+            setting_direction=getch();
+            number++;
+             if(setting_direction==75 || setting_direction==72){
+                if(type==1){number=3;}
+                if(type==2){number=1;}
+                if(type==3){number=2;}
+             }
+             if(number==1){
+                setTextColor (7,0);
+                gotoxy(4,45); printf("Q U O R I D O R   SETTING!");
+                gotoxy(10,41); printf ("'CHOOSE THE BOARD SIZE YOU WANT'") ;
+                setTextColor (7,0); setting_shape(13, 52, 9); gotoxy(15,57); printf("%d", size_board);
+                setTextColor (7,0); setting_shape(13, 63, 9); gotoxy(15,67); printf(">>>");
+                setTextColor (7,0); setting_shape(20, 51, 11); gotoxy(22,56); printf("EXIT");
+                setTextColor (10,0);
+                setting_shape(13, 41, 9); setTextColor (15,0); gotoxy(15,45); printf("<<<");
+                type=1;
+        }
+           if(number==2){
+                setTextColor (7,0);
+                gotoxy(4,45); printf("Q U O R I D O R   SETTING!");
+                gotoxy(10,41); printf ("'CHOOSE THE BOARD SIZE YOU WANT'") ;
+                setting_shape(13, 41, 9); gotoxy(15,45); printf("<<<");
+                setTextColor (7,0); setting_shape(13, 52, 9); gotoxy(15,57); printf("%d", size_board);
+                setTextColor (7,0); setting_shape(20, 51, 11); gotoxy(22,56); printf("EXIT");
+                setTextColor (10,0);
+                setting_shape(13, 63, 9); setTextColor (15,0); gotoxy(15,67); printf(">>>");
+                type=2;
+        }
+           if(number==3){
+                setTextColor (7,0);
+                gotoxy(4,45); printf("Q U O R I D O R   SETTING!");
+                gotoxy(10,41); printf ("'CHOOSE THE BOARD SIZE YOU WANT'") ;
+                setting_shape(13, 41, 9); setTextColor (7,0); gotoxy(15,45); printf("<<<");
+                setTextColor (7,0); setting_shape(13, 52, 9); setTextColor (7,0); gotoxy(15,57); printf("%d", size_board);
+                setting_shape(13, 63, 9); setTextColor (7,0); gotoxy(15,67); printf(">>>");
+                setTextColor (5,0);
+                setting_shape(20, 51, 11); gotoxy(22,56); printf("EXIT");
+                type=3;
+                number=0;
+           }
+        }
+        if(type==1){
+            if(size_board>2) size_board--;
+            if(size_board<10) gotoxy(15,58); printf(" ");
+            setTextColor (7,0); setting_shape(13, 52, 9); setTextColor (4,0); gotoxy(15,57); printf("%d", size_board);
+        }
+       if(type==2){
+            if(size_board<14) size_board++;
+            setTextColor (7,0); setting_shape(13, 52, 9); setTextColor (4,0); gotoxy(15,57); printf("%d", size_board);
+        }
+       if(type==3){
+            TotalColumn=8*size_board-1;
+            TotalRows=4*size_board+1;
+            row1=size_board*4-2;
+            sw_setting=1;
+            system("cls");
+            setting_player();
+        }
+      }while(sw_setting==0);
+}
+
 
 void Design_color_screen()//Choice of screen color by players
 {
-printf ("'SELECT YOUR SCREEN COLOR BY PRESS ENTER THEN PRESS 0'\n") ;
-    while ( getch() == 13 )
+
+    int n=1;
+    char setting_direction;
+    int number=0, type=1, sw_setting=0;
+    setTextColor (7,0);
+    frame(8,23 ,70,10);
+    gotoxy(4,45); printf("Q U O R I D O R   SETTING!");
+    gotoxy(10,32); printf ("'SELECT YOUR SCREEN COLOR BY PRESS ENTER THEN EXIT'") ;
+    setting_shape(13, 25, 17); setTextColor (15,colorscreen); gotoxy(15,30); printf("W H I T E");
+    setTextColor (0,colorscreen); setting_shape(13, 49, 17); setTextColor (11,colorscreen); gotoxy(15,55); printf("B L U E");
+    setTextColor (0,colorscreen);setting_shape(13, 74, 17); setTextColor (14,colorscreen); gotoxy(15,78); printf("Y E L L O W");
+    setTextColor (0,colorscreen); setting_shape(20, 51, 12); gotoxy(22,56); printf("EXIT");
+    do{
+        while(1){
+            setting_direction=getch();
+            if(setting_direction==13) break;
+            setting_direction=getch();
+            number++;
+             if(setting_direction==75 || setting_direction==72){
+                if(type==1){number=4;}
+                if(type==2){number=1;}
+                if(type==3){number=2;}
+                if(type==4){number=3;}
+             }
+            if(number==1){
+                setTextColor (0,colorscreen);
+                gotoxy(4,45); printf("Q U O R I D O R   SETTING!");
+                gotoxy(10,32); printf ("'SELECT YOUR SCREEN COLOR BY PRESS ENTER THEN EXIT'") ;
+                setTextColor (10,colorscreen); setting_shape(13, 25, 17); setTextColor (10,colorscreen); gotoxy(15,30); printf("W H I T E");
+                setTextColor (11,colorscreen); setting_shape(13, 49, 17); setTextColor (0,colorscreen); gotoxy(15,55); printf("B L U E");
+                setTextColor (14,colorscreen);setting_shape(13, 74, 17); setTextColor (0,colorscreen); gotoxy(15,78); printf("Y E L L O W");
+                setTextColor (0,colorscreen); setting_shape(20, 51, 12); gotoxy(22,56); printf("EXIT");
+                type=1;
+            }
+            if(number==2){
+                setTextColor (0,colorscreen);
+                gotoxy(4,45); printf("Q U O R I D O R   SETTING!");
+                gotoxy(10,32); printf ("'SELECT YOUR SCREEN COLOR BY PRESS ENTER THEN EXIT'") ;
+                setTextColor (15,colorscreen); setting_shape(13, 25, 17); setTextColor (0,colorscreen); gotoxy(15,30); printf("W H I T E");
+                setTextColor (10,colorscreen); setting_shape(13, 49, 17); setTextColor (10,colorscreen); gotoxy(15,55); printf("B L U E");
+                setTextColor (14,colorscreen);setting_shape(13, 74, 17); setTextColor (0,colorscreen); gotoxy(15,78); printf("Y E L L O W");
+                setTextColor (0,colorscreen); setting_shape(20, 51, 12); gotoxy(22,56); printf("EXIT");
+                type=2;
+            }
+            if(number==3){
+                setTextColor (0,colorscreen);
+                gotoxy(4,45); printf("Q U O R I D O R   SETTING!");
+                gotoxy(10,32); printf ("'SELECT YOUR SCREEN COLOR BY PRESS ENTER THEN EXIT'") ;
+                setTextColor (15,colorscreen); setting_shape(13, 25, 17); setTextColor (0,colorscreen); gotoxy(15,30); printf("W H I T E");
+                setTextColor (11,colorscreen); setting_shape(13, 49, 17); setTextColor (0,colorscreen); gotoxy(15,55); printf("B L U E");
+                setTextColor (10,colorscreen);setting_shape(13, 74, 17); setTextColor (10,colorscreen); gotoxy(15,78); printf("Y E L L O W");
+                setTextColor (0,colorscreen); setting_shape(20, 51, 12); gotoxy(22,56); printf("EXIT");
+                type=3;
+            }
+            if(number==4){
+                setTextColor (0,colorscreen);
+                gotoxy(4,45); printf("Q U O R I D O R   SETTING!");
+                gotoxy(10,32); printf ("'SELECT YOUR SCREEN COLOR BY PRESS ENTER THEN EXIT'") ;
+                setTextColor (15,colorscreen); setting_shape(13, 25, 17); setTextColor (0,colorscreen); gotoxy(15,30); printf("W H I T E");
+                setTextColor (11,colorscreen); setting_shape(13, 49, 17); setTextColor (0,colorscreen); gotoxy(15,55); printf("B L U E");
+                setTextColor (14,colorscreen);setting_shape(13, 74, 17); setTextColor (0,colorscreen); gotoxy(15,78); printf("Y E L L O W");
+                setTextColor (10,colorscreen); setting_shape(20, 51, 12); gotoxy(22,56); printf("EXIT");
+                type=4;
+                number =0 ;
+            }
+        }
+        if(type==1){
+            system ("COLOR F0") ;
+            colorscreen =15 ;
+        }
+        if(type==2){
+            system ("COLOR B0") ;
+            colorscreen = 11 ;
+        }
+        if(type==3){
+            system ("COLOR E0") ;
+            colorscreen = 14 ;
+        }
+        if(type==4){
+            system("cls");
+            setting_player();
+            sw_setting = 1;
+        }
+    }while(sw_setting==0);
+  /*  while ( getch() == 13 )
         {
          beep (500,100) ;
-         if ( getch() == 13 )
+         if (n!=1){
+            if(getch() == 13)
          {
             system ("COLOR F0") ;
             colorscreen =15 ;
          }
          else if ( getch() != '\n' ) break ;
+         }
          beep (500,100) ;
          if ( getch() == 13 )
          {
@@ -29,6 +196,7 @@ printf ("'SELECT YOUR SCREEN COLOR BY PRESS ENTER THEN PRESS 0'\n") ;
          }
          else if ( getch() != '\n' ) break ;
          beep (500,100) ;
+         n++;
     }
 }
 
@@ -56,7 +224,7 @@ void loading(){
      }
 
     	printslowly("COMPLETE !!! ",16,64);
-    	sleep(50);
+    	sleep(50);*/
 }
 
 int logo(int TotalRows,int TotalColumn){
@@ -160,7 +328,15 @@ void printwon(char name[])
     setTextColor (10, 15) ;
     for (n=0; n<=179; n++)
     {
-        printf (" Congratulations, %s won ! ", name);
-        sleep (30) ;
+        if(turn != 3){
+           printf (" Congratulations, %s won ! ", name);
+           sleep (30) ;
+        }
+        else{
+            setTextColor (12, 15) ;
+            printf (" Sorry, you LOST !");
+           sleep (30) ;
+        }
     }
 }
+#endif // design_screen
